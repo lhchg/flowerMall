7b823301341600dcf2cd8d2f6e822ff404a76311
Haocheng <lhao_cheng@163.com>
2024-02-19 22:20:25 +0800

    add account page

--- www.py
+++ www.py
@@ -1,8 +1,10 @@
 from web.controllers.index import route_index
 from web.controllers.user.User import route_user
 from web.controllers.static import route_static
+from web.controllers.account.account import route_account
 from application import app
 
 app.register_blueprint(route_index, url_prefix="/")
 app.register_blueprint(route_user, url_prefix="/user")
 app.register_blueprint(route_static, url_prefix="/static")
+app.register_blueprint(route_account, url_prefix="/account")
