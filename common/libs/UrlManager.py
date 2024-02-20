41b358e9f85f92b655d4b9f494dff0fb9de834df
Haocheng <lhao_cheng@163.com>
2024-02-20 22:00:07 +0800

    change template file

--- common/libs/UrlManager.py
+++ common/libs/UrlManager.py
@@ -1,6 +1,5 @@
 # -*- coding: utf-8 -*-
-import time
-from application import  app
+
 class UrlManager(object):
     def __init__(self):
         pass
@@ -11,13 +10,6 @@ class UrlManager(object):
 
     @staticmethod
     def buildStaticUrl(path):
-        release_version = app.config.get( 'RELEASE_VERSION' )
-        ver = "%s"%( int( time.time() ) ) if not release_version else release_version
+        ver = "%s"%( 22222222 )
         path =  "/static" + path + "?ver=" + ver
         return UrlManager.buildUrl( path )
-
-    @staticmethod
-    def buildImageUrl( path ):
-        app_config = app.config['APP']
-        url = app_config['domain'] + app.config['UPLOAD']['prefix_url'] + path
-        return url
