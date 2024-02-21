15ee210c723d5f51256ad833ae40c853d5a54268
Haocheng <lhao_cheng@163.com>
2024-02-21 17:26:59 +0800

    return to login page if not login

--- config/base_setting.py
+++ config/base_setting.py
@@ -4,3 +4,12 @@ DEBUG = False
 SQLALCHEMY_ECHO = False
 
 AUTH_COOKIE_NAME = "food"
+
+IGNORE_URL = [
+    "^/user/login"
+]
+
+IGNORE_CHECK_LOGIN_URLS = [
+    "^/static",
+    "^/favicon.ico"
+]
--- www.py
+++ www.py
@@ -1,3 +1,5 @@
+from web.interceptors.AutoInterceptor import *
+
 from web.controllers.index import route_index
 from web.controllers.static import route_static
 from web.controllers.user.User import route_user
@@ -7,6 +9,7 @@ from web.controllers.food.Food import route_food
 from web.controllers.member.Member import route_member
 from web.controllers.stat.Stat import route_stat
 from application import app
+
 
 app.register_blueprint(route_index, url_prefix="/")
 app.register_blueprint(route_static, url_prefix="/static")
