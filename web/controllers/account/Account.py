df5485122e04fde6e41a80dd4409ce23bd2e8b94
Haocheng <lhao_cheng@163.com>
2024-02-22 14:11:41 +0800

    [function] account list show

--- web/controllers/account/Account.py
+++ web/controllers/account/Account.py
@@ -1,13 +1,18 @@
 # -*- coding: utf-8 -*-
 from flask import Blueprint
 from common.libs.Helper import ops_render
+from common.models.User import User
 
 route_account = Blueprint('account_page', __name__)
 
 
 @route_account.route("/index")
 def index():
-    return ops_render("account/index.html")
+    resp_data = {}
+    usr_list = User.query.order_by(User.uid.desc()).all()
+    resp_data['list'] = usr_list
+
+    return ops_render("account/index.html", resp_data)
 
 
 @route_account.route("/info")
