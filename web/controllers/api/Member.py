f47a170c90cfc52adada076127d96df67d50cf86
Haocheng <lhao_cheng@163.com>
2024-03-07 16:18:14 +0800

    [function] add mina login check

--- web/controllers/api/Member.py
+++ web/controllers/api/Member.py
@@ -7,6 +7,7 @@ import json
 from common.models.member.Member import Member
 from common.models.member.OauthMemberBind import OauthMemberBind
 from common.libs.Helper import getCurrentDate
+from common.libs.member.MemberService import MemberService
 
 @route_api.route("/member/login", methods=["GET", "POST"])
 def login():
@@ -20,13 +21,11 @@ def login():
         resp['msg'] = "需要code"
         return json.dumps(resp, ensure_ascii=False)
 
-    url = ("https://api.weixin.qq.com/sns/jscode2session?appid={0}&secret={1}&js_code={2}&grant_type=authorization_code"
-           .format(app.config['MINA_APP']['appid'], app.config['MINA_APP']['appkey'], code))
-
-    r = requests.get(url)
-    res = json.loads(r.text)
-    app.logger.info(res)
-    openid = res['openid']
+    openid = MemberService.getWechatOpenId(code)
+    if openid is None:
+        resp['code'] = -1
+        resp['msg'] = "调用出错"
+        return json.dumps(resp, ensure_ascii=False)
 
     nickname = req['nickname'] if 'nickname' in req else ""
     sex = req['gender'] if 'gender' in req else 0
@@ -38,7 +37,7 @@ def login():
         model_member.nickname = nickname
         model_member.sex = sex
         model_member.avatar = avatar
-        model_member.salt = ""
+        model_member.salt = MemberService.geneSalt()
         model_member.updated_time = model_member.created_time = getCurrentDate()
         db.session.add(model_member)
         db.session.commit()
@@ -55,10 +54,41 @@ def login():
         bind_info = model_bind
 
     member_info = Member.query.filter_by(id=bind_info.member_id).first()
-    resp['data'] = {'nickname': member_info.nickname}
+    token = "%s#%s" % (MemberService.geneAuthCode(member_info), member_info.id)
+    resp['data'] = {'token': token}
     return json.dumps(resp, ensure_ascii=False)
 
 
+@route_api.route("/member/check-reg", methods=["GET", "POST"])
+def checkReg():
+    resp = {'code': 200, 'msg': '操作成功', 'data': {}}
+    req = request.values
+    app.logger.info(req)
+    code = req['code'] if 'code' in req else ''
+    if not code or len(code) < 1:
+        resp['code'] = -1
+        resp['msg'] = "需要code"
+        return json.dumps(resp, ensure_ascii=False)
 
+    openid = MemberService.getWechatOpenId(code)
+    if openid is None:
+        resp['code'] = -1
+        resp['msg'] = "调用出错"
+        return json.dumps(resp, ensure_ascii=False)
 
+    bind_info = OauthMemberBind.query.filter_by(openid=openid, type=1).first()
+    if not bind_info:
+        resp['code'] = -1
+        resp['msg'] = "未绑定"
+        return json.dumps(resp, ensure_ascii=False)
 
+    member_info = Member.query.filter_by(id=bind_info.member_id).first()
+    if not member_info:
+        resp['code'] = -1
+        resp['msg'] = "未查询到绑定信息"
+        return json.dumps(resp, ensure_ascii=False)
+
+    token = "%s#%s" % (MemberService.geneAuthCode(member_info), member_info.id)
+    resp['data'] = {'token': token}
+
+    return json.dumps(resp, ensure_ascii=False)
