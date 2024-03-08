c68ba3c09af4373f81f62297b9fc0f9d91c7ca97
Haocheng <lhao_cheng@163.com>
2024-03-08 14:45:09 +0800

    [feature] add mina user list show

--- web/controllers/member/Member.py
+++ web/controllers/member/Member.py
@@ -1,13 +1,39 @@
 # -*- coding: utf-8 -*-
-from flask import Blueprint
-from common.libs.Helper import ops_render
+from flask import Blueprint, request
+from common.libs.Helper import ops_render, iPagination
+from common.models.member.Member import Member
+from application import app
 
 route_member = Blueprint('member_page', __name__)
 
 
 @route_member.route("/index")
 def index():
-    return ops_render("member/index.html")
+    resp_data = {}
+
+    req = request.values
+    page = int(req['p']) if ('p' in req and req['[']) else 1
+    query = Member.query
+
+    page_params = {
+        'total': query.count(),
+        'page_size': app.config['PAGE_SIZE'],
+        'page': page,
+        'display': app.config['PAGE_DISPLAY'],
+        'url': request.full_path.replace("&p={}".format(page), "")
+    }
+
+    pages = iPagination(page_params)
+    offset = (page - 1) * app.config['PAGE_SIZE']
+    list = query.order_by(Member.id.desc()).offset(offset).limit(app.config['PAGE_SIZE']).all()
+
+    resp_data['list'] = list
+    resp_data['pages'] = pages
+    resp_data['search_con'] = req
+    resp_data['status_mapping'] = app.config['STATUS_MAPPING']
+    resp_data['current'] = "index"
+
+    return ops_render("member/index.html", resp_data)
 
 
 @route_member.route("/info")
